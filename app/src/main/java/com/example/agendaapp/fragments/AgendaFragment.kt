efa2eeaaae95d6dc894ca316c45d99aeffa30af8
Goje Andrei <andreigoja1@gmail.com>
2022-09-17 15:40:40 +0200

    Moved the logic part into the view model

--- app/src/main/java/com/example/agendaapp/fragments/AgendaFragment.kt
+++ app/src/main/java/com/example/agendaapp/fragments/AgendaFragment.kt
@@ -1,12 +1,8 @@
 package com.example.agendaapp.fragments
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.ContentResolver
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -14,17 +10,20 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.agendaapp.databinding.FragmentAgendaBinding
 
 
 class AgendaFragment : Fragment() {
 
-    private lateinit var binding: FragmentAgendaBinding
+    private var binding: FragmentAgendaBinding? = null
 
-    private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) {}
+    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
+    private val sharedViewModel: AgendaViewModel by activityViewModels()
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,8 +36,13 @@ class AgendaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
         checkPermissionToReadContacts()
-        Log.e("CONTATS", getContacts().toString())
+        Log.e("CONTACTS", sharedViewModel.contactsList.value.toString())
     }
 
     private fun checkPermissionToReadContacts() {
@@ -48,10 +52,9 @@ class AgendaFragment : Fragment() {
 
     private fun userHasPermission(): Boolean {
         return (ActivityCompat.checkSelfPermission(
-            binding.root.context,
+            binding?.root?.context!!,
             Manifest.permission.READ_CONTACTS
-        )
-                == PackageManager.PERMISSION_GRANTED)
+        ) == PackageManager.PERMISSION_GRANTED)
     }
 
     private fun requestPermission() {
@@ -60,49 +63,9 @@ class AgendaFragment : Fragment() {
         )
     }
 
-    @SuppressLint("Range")
-    private fun getContacts(): ArrayList<Contact> {
-        val contentResolver: ContentResolver = binding.root.context.contentResolver
-        var contactId: String?
-        val contactsInfoList = ArrayList<Contact>()
-        val cursor: Cursor? = contentResolver.query(
-            ContactsContract.Contacts.CONTENT_URI,
-            null,
-            null,
-            null,
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
-        )
-        if (cursor?.count!! > 0) {
-            while (cursor.moveToNext()) {
-                val hasPhoneNumber: Int =
-                    cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
-                        .toInt()
-                if (hasPhoneNumber > 0) {
-                    contactId =
-                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
-                    val phoneCursor: Cursor = contentResolver.query(
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                        null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                        arrayOf<String?>(contactId),
-                        null
-                    )!!
-                    if (phoneCursor.moveToNext()) {
-                        val phoneNumber: String =
-                            phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                        contactsInfoList.add(
-                            Contact(
-                                phoneNumber,
-                                contactId
-                            )
-                        )
-                    }
-                    phoneCursor.close()
-                }
-            }
-        }
-        cursor.close()
-
-        return contactsInfoList
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
+
 }
--- app/src/main/java/com/example/agendaapp/fragments/AgendaViewModel.kt
+++ app/src/main/java/com/example/agendaapp/fragments/AgendaViewModel.kt
@@ -1,25 +1,72 @@
 package com.example.agendaapp.fragments
 
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.ContentResolver
 import android.database.Cursor
-import android.os.Bundle
-import androidx.lifecycle.ViewModel
-
 import android.provider.ContactsContract
-import androidx.loader.app.LoaderManager
-import androidx.loader.content.Loader
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.agendaapp.objects.Contact
 
-class AgendaViewModel : ViewModel(), LoaderManager.LoaderCallbacks<Cursor> {
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        TODO("Not yet implemented")
+class AgendaViewModel(application: Application) : AndroidViewModel(application) {
+
+    // Cupcake flavor for this order
+    private var _contactsList = MutableLiveData<ArrayList<Contact>>()
+    val contactsList: LiveData<ArrayList<Contact>> = _contactsList
+
+
+    init {
+        getContacts()
     }
 
-    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        TODO("Not yet implemented")
+    @SuppressLint("Range")
+    private fun getContacts() {
+        val contentResolver: ContentResolver = getApplication<Application>().contentResolver
+        var contactId: String?
+        val contactsInfoList = ArrayList<Contact>()
+        val cursor: Cursor? = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+        )
+        if (cursor?.count!! > 0) {
+            while (cursor.moveToNext()) {
+                val hasPhoneNumber: Int =
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
+                        .toInt()
+                if (hasPhoneNumber > 0) {
+                    contactId =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+
+                    val displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+
+                    val phoneCursor: Cursor = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        arrayOf<String?>(contactId),
+                        null
+                    )!!
+                    if (phoneCursor.moveToNext()) {
+                        val phoneNumber: String =
+                            phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                        contactsInfoList.add(
+                            Contact(
+                                phoneNumber,
+                                displayName
+                            )
+                        )
+                    }
+                    phoneCursor.close()
+                }
+            }
+        }
+        cursor.close()
+
+        _contactsList.value = contactsInfoList
     }
-
-    override fun onLoaderReset(loader: Loader<Cursor>) {
-        TODO("Not yet implemented")
-    }
-
-
 }
