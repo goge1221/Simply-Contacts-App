c240c5024a05bc85d7ececc7e93031d507658b35
Goje Andrei <andreigoja1@gmail.com>
2022-10-14 13:55:10 +0200

    Tab layout and view pager now completed.

--- app/src/main/java/com/example/agendaapp/MainActivity.kt
+++ app/src/main/java/com/example/agendaapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.agendaapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.example.agendaapp.databinding.ActivityMainBinding
 import com.example.agendaapp.viewPagerAdapter.ViewPager2Adapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     
@@ -18,7 +20,30 @@ class MainActivity : AppCompatActivity() {
         binding?.apply {
             viewPager.adapter = ViewPager2Adapter(this@MainActivity, 2)
         }
-
+        addOnTabSelectedListener()
+        addOnViewPagerChangedListener()
     }
+
+    private fun addOnTabSelectedListener(){
+        binding?.tabLayout?.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding?.viewPager?.currentItem = tab!!.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
+
+
+    private fun addOnViewPagerChangedListener(){
+        binding?.viewPager?.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding?.tabLayout?.getTabAt(position)?.select()
+            }
+        })
+    }
+
+
 
 }
