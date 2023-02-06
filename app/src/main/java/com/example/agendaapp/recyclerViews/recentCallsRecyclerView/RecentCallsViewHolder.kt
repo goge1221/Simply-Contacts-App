208220ffa49e80cc610d9f3d046bf8924e2df776
Goje Andrei <a12032793@univie.ac.at>
2023-02-06 17:25:09 +0100

    Created the base look of a single recent call by adding the call type visually in form of an vector.

--- app/src/main/java/com/example/agendaapp/recyclerViews/recentCallsRecyclerView/RecentCallsViewHolder.kt
+++ app/src/main/java/com/example/agendaapp/recyclerViews/recentCallsRecyclerView/RecentCallsViewHolder.kt
@@ -1,6 +1,8 @@
 package com.example.agendaapp.recyclerViews.recentCallsRecyclerView
 
+import android.provider.CallLog
 import androidx.recyclerview.widget.RecyclerView
+import com.example.agendaapp.R
 import com.example.agendaapp.databinding.SingleRecentCallItemBinding
 import com.example.agendaapp.entity.RecentCall
 
@@ -8,9 +10,23 @@ class RecentCallsViewHolder(
     private val binding: SingleRecentCallItemBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-
     fun bind(recentCall: RecentCall) {
         binding.callerName.text = recentCall.name
+        determineCallType(recentCall.type)
     }
+
+    private fun determineCallType(callType : Int){
+        when(callType){
+            CallLog.Calls.OUTGOING_TYPE -> setIconWithDrawable(R.drawable.baseline_call_made_24)
+            CallLog.Calls.INCOMING_TYPE -> setIconWithDrawable(R.drawable.baseline_call_received_24)
+            CallLog.Calls.MISSED_TYPE -> setIconWithDrawable(R.drawable.baseline_call_missed_24)
+            CallLog.Calls.REJECTED_TYPE -> setIconWithDrawable(R.drawable.baseline_call_missed_outgoing_24)
+        }
+    }
+
+    private fun setIconWithDrawable(drawableInt : Int){
+        binding.callTypeImageView.foreground = binding.root.resources.getDrawable(drawableInt)
+    }
+
 
 }
