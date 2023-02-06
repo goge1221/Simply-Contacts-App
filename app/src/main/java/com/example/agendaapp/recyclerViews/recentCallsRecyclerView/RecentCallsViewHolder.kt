58d20e60a7175ca5a6dfc5ef8afe324dd0420547
Goje Andrei <a12032793@univie.ac.at>
2023-02-06 18:30:43 +0100

    Formated the cells to look better.

--- app/src/main/java/com/example/agendaapp/recyclerViews/recentCallsRecyclerView/RecentCallsViewHolder.kt
+++ app/src/main/java/com/example/agendaapp/recyclerViews/recentCallsRecyclerView/RecentCallsViewHolder.kt
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.agendaapp.R
 import com.example.agendaapp.databinding.SingleRecentCallItemBinding
 import com.example.agendaapp.entity.RecentCall
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RecentCallsViewHolder(
     private val binding: SingleRecentCallItemBinding
@@ -13,10 +15,26 @@ class RecentCallsViewHolder(
     fun bind(recentCall: RecentCall) {
         binding.callerName.text = recentCall.name
         determineCallType(recentCall.type)
+        setTime(recentCall.duration)
+        setDate(recentCall.date)
     }
 
-    private fun determineCallType(callType : Int){
-        when(callType){
+    private fun setTime(timeInMinutes: Int) {
+        val callDuration: String = binding.root.resources.getString(
+            R.string.call_time, timeInMinutes.toString()
+        )
+        binding.duration.text = callDuration
+    }
+
+    private fun setDate(receivedDate: Date) {
+        val simpleDateFormat = SimpleDateFormat.getDateInstance()
+        val date: String = simpleDateFormat.format(receivedDate)
+        binding.date.text = date
+    }
+
+
+    private fun determineCallType(callType: Int) {
+        when (callType) {
             CallLog.Calls.OUTGOING_TYPE -> setIconWithDrawable(R.drawable.baseline_call_made_24)
             CallLog.Calls.INCOMING_TYPE -> setIconWithDrawable(R.drawable.baseline_call_received_24)
             CallLog.Calls.MISSED_TYPE -> setIconWithDrawable(R.drawable.baseline_call_missed_24)
@@ -24,7 +42,7 @@ class RecentCallsViewHolder(
         }
     }
 
-    private fun setIconWithDrawable(drawableInt : Int){
+    private fun setIconWithDrawable(drawableInt: Int) {
         binding.callTypeImageView.foreground = binding.root.resources.getDrawable(drawableInt)
     }
 
