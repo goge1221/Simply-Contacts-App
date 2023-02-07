4be0e40a60ccc7fa596d9efac0691106a2f6a2fe
Goje Andrei <a12032793@univie.ac.at>
2023-02-07 13:52:55 +0100

    Removed the unnecessary permissions from manifest file + reduces the distance and size of the button and added the on click listener in order to ask the user about the permission if they click the button.

--- app/src/main/java/com/example/agendaapp/MainActivity.kt
+++ app/src/main/java/com/example/agendaapp/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.agendaapp
 
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.agendaapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val PERMISSION_TO_READ_CONTACTS = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,43 +17,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (hasPermissionToReadContacts()) {
-         //   val navView: BottomNavigationView = binding.navView
-         //   val navController = findNavController(R.id.nav_host_fragment_activity_main2)
-          //  navView.setupWithNavController(navController)
-        } else requestPermissionToReadContacts()
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main2)
+        navView.setupWithNavController(navController)
     }
-
-    private fun hasPermissionToReadContacts() = ActivityCompat.checkSelfPermission(
-        this@MainActivity,
-        android.Manifest.permission.READ_CONTACTS
-    ) == PackageManager.PERMISSION_GRANTED
-
-
-    private fun requestPermissionToReadContacts() {
-        if (!hasPermissionToReadContacts()){
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.READ_CONTACTS),
-                PERMISSION_TO_READ_CONTACTS
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_TO_READ_CONTACTS && grantResults.isNotEmpty()){
-            for (i in grantResults.indices){
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
-                    Log.i("PERMISSION_READ_STORAGE", "${permissions[i]} granted.")
-                }
-            }
-        }
-    }
-
 
 }
--- app/src/main/java/com/example/agendaapp/ui/agenda/AgendaFragment.kt
+++ app/src/main/java/com/example/agendaapp/ui/agenda/AgendaFragment.kt
@@ -1,9 +1,11 @@
 package com.example.agendaapp.ui.agenda
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.agendaapp.databinding.FragmentAgendaBinding
@@ -16,7 +18,6 @@ class AgendaFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
     private lateinit var agendaViewModel: AgendaViewModel
 
     override fun onCreateView(
@@ -24,17 +25,33 @@ class AgendaFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        agendaViewModel = ViewModelProvider(this)[AgendaViewModel::class.java]
         _binding = FragmentAgendaBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initializeRecyclerView()
+        if (hasPermissionToReadContacts()) {
+            initializeViewModel()
+            changeLayoutToPermissionsGranted()
+        } else {
+            requestPermissionToReadContacts()
+            setGrantPermissionsButtonListener()
+        }
+
     }
 
-    private fun initializeRecyclerView(){
+    private fun initializeViewModel() {
+        agendaViewModel = ViewModelProvider(this)[AgendaViewModel::class.java]
+    }
+
+    private fun initializePermissionsNotGrantedLayout() {
+        binding.linearLayout.visibility = View.VISIBLE
+    }
+
+    private fun changeLayoutToPermissionsGranted() {
+        binding.linearLayout.visibility = View.GONE
+        binding.agendaRecyclerView.visibility = View.VISIBLE
         binding.agendaRecyclerView.adapter = AgendaAdapter(agendaViewModel.contactsList.value!!)
     }
 
@@ -42,4 +59,49 @@ class AgendaFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun setGrantPermissionsButtonListener() {
+        binding.grantPermissionsButton.setOnClickListener {
+            requestPermissionToReadContacts()
+        }
+    }
+
+    private fun hasPermissionToReadContacts() = ActivityCompat.checkSelfPermission(
+        requireContext(),
+        android.Manifest.permission.READ_CONTACTS
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun requestPermissionToReadContacts() {
+        if (!hasPermissionToReadContacts()) {
+            requestPermissions(
+                arrayOf(android.Manifest.permission.READ_CONTACTS),
+                PERMISSION_TO_READ_CONTACTS
+            )
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_TO_READ_CONTACTS && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    //Permission to read contacts was granted
+                    initializeViewModel()
+                    changeLayoutToPermissionsGranted()
+                    return
+                }
+            }
+        }
+        initializePermissionsNotGrantedLayout()
+    }
+
+    companion object {
+        private const val PERMISSION_TO_READ_CONTACTS = 0
+    }
+
 }
