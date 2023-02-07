96a951a5469886c25566ff24c2c72ce6adffbe33
Goje Andrei <a12032793@univie.ac.at>
2023-02-07 12:38:40 +0100

    Created a permission handler which asks for the permission to read contacts.

--- app/src/main/java/com/example/agendaapp/MainActivity.kt
+++ app/src/main/java/com/example/agendaapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.agendaapp
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.agendaapp.databinding.ActivityMainBinding
@@ -10,6 +13,7 @@ import com.example.agendaapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val PERMISSION_TO_READ_CONTACTS = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +21,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_activity_main2)
-        navView.setupWithNavController(navController)
+        if (hasPermissionToReadContacts()) {
+         //   val navView: BottomNavigationView = binding.navView
+         //   val navController = findNavController(R.id.nav_host_fragment_activity_main2)
+          //  navView.setupWithNavController(navController)
+        } else requestPermissionToReadContacts()
     }
+
+    private fun hasPermissionToReadContacts() = ActivityCompat.checkSelfPermission(
+        this@MainActivity,
+        android.Manifest.permission.READ_CONTACTS
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun requestPermissionToReadContacts() {
+        if (!hasPermissionToReadContacts()){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_CONTACTS),
+                PERMISSION_TO_READ_CONTACTS
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_TO_READ_CONTACTS && grantResults.isNotEmpty()){
+            for (i in grantResults.indices){
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Log.i("PERMISSION_READ_STORAGE", "${permissions[i]} granted.")
+                }
+            }
+        }
+    }
+
+
 }
