2fc3ffc43b022514b0d7cbfab067932207455516
Goje Andrei <a12032793@univie.ac.at>
2023-08-13 12:22:13 +0300

    Solved bug where pressing the back button would not leave the app.

--- app/src/main/java/goje/contactsapp/MainActivity.kt
+++ app/src/main/java/goje/contactsapp/MainActivity.kt
@@ -2,7 +2,6 @@ package goje.contactsapp
 
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import goje.contactsapp.databinding.ActivityMainBinding
 import goje.contactsapp.utils.Constants
@@ -17,8 +16,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        whenBackButtonClickedReturnToAgendaFragment()
-
         val c: Configuration = resources.configuration
         val scale: Float = c.fontScale
         Constants.fontSize = scale
@@ -28,16 +25,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun whenBackButtonClickedReturnToAgendaFragment() {
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-
-                supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main2)
-
-                supportFragmentManager.popBackStack()
-            }
-        })
-    }
 
 
 }
